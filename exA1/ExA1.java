0e747b6367d686df41a637bacce212648c063db8
Sandy <sschillebeeckx@abis.be>
2020-09-24 10:24:36 +0200

    A1

---
+++ exA1/ExA1.java
@@ -0,0 +1,16 @@
+public class ExA1 {
+	
+	public static void main (String[] args) {
+		
+		String title = "Java Programming";
+		int numberOfDays = 2;
+		double pricePerDay = 450.5;
+		boolean priorKnowledgeRequired = false;
+		
+		System.out.println("The " + title + " course takes " + numberOfDays + " days and costs " + pricePerDay + " euros per day. \nPrior knowledge required: " + priorKnowledgeRequired );
+		
+	
+		
+		
+	}
+}
